695dd297680db5f477311673d1dbedb4f29cb534
Irham <irhamputra35@gmail.com>
2025-06-01 21:14:05 +0700

    empty

--- src/Main.java
+++ src/Main.java
@@ -5,13 +5,6 @@ import Benda3D.*;
 
 public class Main {
     public static void main(String[] args) {
-        BendaGeometri p1 = new Persegi(20);
-        BendaGeometri p2 = new PersegiPanjang(12, 8);
 
-        Thread t1 = new Thread(p1);
-        Thread t2 = new Thread((Runnable) p2);
-
-        t1.start();
-        t2.start();
     }
 }
